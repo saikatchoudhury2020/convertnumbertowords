cc1deeb6830bcb86a688413d7cb6079f2d620c58
saikatchoudhury2020 <saikathchowdhury7@gmail.com>
2020-05-26 08:26:49 +0530

    code modify
    code modify

---
+++ convertnumbertoword/convertnumericwords/convertnumbertospellswords.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace convertnumbertoword.convertnumericwords
+{
+    public class convertnumbertospellswords
+    {
+
+        private static String ConvertDecimals(String number)
+        {
+            String cd = "", digit = "", engOne = "";
+            for (int i = 0; i < number.Length; i++)
+            {
+                digit = number[i].ToString();
+                if (digit.Equals("0"))
+                {
+                    engOne = "Zero";
+                }
+                else
+                {
+                    engOne = ones(digit);
+                }
+                cd += " " + engOne;
+            }
+            return cd;
+        }
+    }
+}
